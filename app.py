90d59b289a6b181d4de5a74b50624b0f03541d72
anamariastefan <67341798+anamariastefan@users.noreply.github.com>
2021-12-28 23:22:56 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,71 @@
+﻿from __future__ import division, print_function
+
+import sys
+import os
+import glob
+import re
+import numpy as np
+
+from keras.applications.imagenet_utils import preprocess_input, decode_predictions
+from keras.models import load_model
+from keras.preprocessing import image
+
+
+from flask import Flask, redirect, url_for, request, render_template
+from werkzeug.utils import secure_filename
+from gevent.wsgi import WSGIServer
+
+
+app = Flask(__name__)
+
+
+MODEL_PATH = 'models/keras_model.h5'
+
+model = load_model(MODEL_PATH)
+        
+print('Model loaded. Start serving...')
+
+
+def model_predict(img_path, model):
+    img = image.load_img(img_path, target_size=(64, 64)) 
+
+
+    img = image.img_to_array(img)
+    img = np.expand_dims(img, axis=0)
+
+   
+    preds = model.predict(img)
+    return preds
+
+
+@app.route('/', methods=['GET'])
+def index():
+
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+      
+        f = request.files['file']
+
+    
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+
+        preds = model_predict(file_path, model)
+        os.remove(file_path)
+
+        str1 = 'Pathology'
+        str2 = 'Normal'
+        if preds == 1:
+            return str1
+        else:
+            return str2
+    return None
+
+if __name__ == '__main__':
+        app.run()
